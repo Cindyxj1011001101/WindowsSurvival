1481e39b87d3de6b4e963ab9ee55f747fae016f4
liangyufeng <2085462714@qq.com>
2025-07-07 11:13:48 +0800

    新增窗口组类，用于管理窗口的显示顺序

---
+++ Assets/Scripts/UI/Screen/Windows/WindowGroup.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class WindowGroup : MonoBehaviour
+{
+    private Transform closed;
+    private Transform minimized;
+    private Transform opened;
+
+    private void Start()
+    {
+        closed = transform.Find("Closed");
+        minimized = transform.Find("Minimized");
+        opened = transform.Find("Opened");
+    }
+
+    public void FocusWindow(WindowBase window)
+    {
+        window.transform.SetParent(opened);
+        window.transform.SetAsLastSibling();
+    }
+
+    public void CloseWindow(WindowBase window)
+    {
+        window.transform.SetParent(closed);
+    }
+
+    public void MinimizeWindow(WindowBase window)
+    {
+        window.transform.SetParent(minimized);
+    }
+
+    public WindowBase GetTheFrontWindow()
+    {
+        if (opened.childCount == 0)
+            return null;
+        return opened.GetChild(opened.childCount - 1).GetComponent<WindowBase>();
+    }
+}
