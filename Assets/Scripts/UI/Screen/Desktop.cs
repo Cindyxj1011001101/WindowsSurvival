3645e5459de0e6d7c35109fe738d2e25fed1d832
liangyufeng <2085462714@qq.com>
2025-07-07 11:12:04 +0800

    新增桌面相关脚本

---
+++ Assets/Scripts/UI/Screen/Desktop.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Desktop : MonoBehaviour
+{
+    private Transform layoutTransform;
+
+    private List<DesktopShortcut> shortcuts = new List<DesktopShortcut>();
+
+    private void Start()
+    {
+        layoutTransform = GetComponentInChildren<GridLayoutGroup>().transform;
+    }
+
+    public void Init(List<App> appsData)
+    {
+        foreach (var app in appsData)
+        {
+            AddShortcut(app);
+        }
+    }
+
+    public void AddShortcut(App app)
+    {
+        if (ShortcutExists(app.name)) return;
+
+        GameObject shortcutPrefab = Resources.Load<GameObject>("Prefabs/UI/Controls/DesktopShortcut");
+        DesktopShortcut shortcut = Instantiate(shortcutPrefab, layoutTransform).GetComponent<DesktopShortcut>();
+        shortcut.Init(app, this);
+        shortcuts.Add(shortcut);
+    }
+
+    public void SelectAppShortcut(string appName)
+    {
+        foreach (var shortcut in shortcuts)
+        {
+            shortcut.SetSelected(appName == shortcut.AppName);
+        }
+    }
+
+    public void ClearSelection()
+    {
+        foreach (var shortcut in shortcuts)
+        {
+            shortcut.SetSelected(false);
+        }
+    }
+
+    private bool ShortcutExists(string appName)
+    {
+        return shortcuts.Find(shortcut => shortcut.AppName == appName);
+    }
+}
---
+++ Assets/Scripts/UI/Screen/DesktopShortcut.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+/// <summary>
+/// 桌面快捷方式
+/// </summary>
+public class DesktopShortcut : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    private Desktop desktop;
+
+    [SerializeField]
+    private Image appIconImage;
+    [SerializeField]
+    private Text appDisplayText;
+
+    private bool selected = false;
+
+    private string appName;
+    public string AppName => appName;
+
+    // 双击检测相关变量
+    private float doubleClickTimeThreshold = 0.3f; // 双击时间间隔阈值
+    private int clickCount = 0;
+    private float lastClickTime = 0;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Init(App app, Desktop desktop)
+    {
+        this.desktop = desktop;
+        appName = app.name;
+        appIconImage.sprite = app.icon;
+        appDisplayText.text = app.displayText;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // 选中被点击的快捷方式
+        desktop.SelectAppShortcut(appName);
+
+        // 检测双击
+        if (Time.time - lastClickTime < doubleClickTimeThreshold)
+        {
+            clickCount++;
+
+            // 如果是第二次点击且在时间阈值内，则视为双击
+            if (clickCount >= 2)
+            {
+                HandleDoubleClick();
+                clickCount = 0;
+            }
+        }
+        else
+        {
+            clickCount = 1;
+        }
+
+        lastClickTime = Time.time;
+    }
+
+    private void HandleDoubleClick()
+    {
+        // 双击打开窗口
+        WindowsManager.Instance.OpenWindow(appName);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (!selected)
+            animator.SetTrigger("Highlight");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (!selected)
+            animator.SetTrigger("Normal");
+    }
+
+    // 不要自己调用
+    // 不要自己调用
+    // 不要自己调用
+    public void SetSelected(bool selected)
+    {
+        if (this.selected == selected) return;
+
+        this.selected = selected;
+        if (selected)
+        {
+            animator.SetTrigger("Select");
+        }
+        else
+        {
+            animator.SetTrigger("Normal");
+        }
+    }
+}
