20c4ac31b0c443ae77de3c295ddedccd7d3b8eca
liangyufeng <2085462714@qq.com>
2025-07-08 14:45:16 +0800

    新增食物卡牌和资源卡牌实例类

---
+++ Assets/Scripts/Card/FoodCardInstance.cs
@@ -0,0 +1,18 @@
+﻿public class FoodCardInstance : CardInstance
+{
+    private int currentFresh;
+
+    public int CurrentFresh => currentFresh;
+
+    public override int CompareTo(CardInstance other)
+    {
+        // 优先级为新鲜度低的优先级高
+        if (other is FoodCardInstance) return currentFresh - (other as FoodCardInstance).currentFresh;
+        return 0;
+    }
+
+    public override void InitFromCardData(CardData cardData)
+    {
+        currentFresh = (cardData as FoodCardData).MaxFresh;
+    }
+}
---
+++ Assets/Scripts/Card/ResourceCardInstance.cs
@@ -0,0 +1,12 @@
+﻿public class ResourceCardInstance : CardInstance
+{
+    public override int CompareTo(CardInstance other)
+    {
+        return 0;
+    }
+
+    public override void InitFromCardData(CardData cardData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
