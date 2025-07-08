80e1f6c3c56bcf13a9a927a1c6ce1241be183a1c
liangyufeng <2085462714@qq.com>
2025-07-08 14:44:17 +0800

    新增玩家背包与环境背包类，继承背包基类

---
+++ Assets/Scripts/Bag/EnvironmentBag.cs
@@ -0,0 +1,13 @@
+﻿public class EnvironmentBag : BagBase
+{
+    public override void AddCard(CardInstance card)
+    {
+        // 如果放不下，就新增格子
+        if (!CanAddCard(card))
+        {
+            // 暂定每次新增3个格子
+            AddSlot(5);
+            base.AddCard(card);
+        }
+    }
+}
