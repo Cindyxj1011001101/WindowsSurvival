b1b7e2d828eaabf81335fd162651646c8c5c47df
liangyufeng <2085462714@qq.com>
2025-07-08 14:46:09 +0800

    新增负重改变事件类型

--- Assets/Scripts/EventManager/EventArgs.cs
+++ Assets/Scripts/EventManager/EventArgs.cs
@@ -1,10 +1,16 @@
 ﻿public enum EventType
 {
-    ChangeState
+    ChangeState,
+    ChangeLoad, // 背包载重变化
 }
 
 public class ChangeStateArgs
 {
     public StateEnum state;
     public int value;
+}
+
+public class ChangeLoadArgs
+{
+    public float currentLoad;
 }
