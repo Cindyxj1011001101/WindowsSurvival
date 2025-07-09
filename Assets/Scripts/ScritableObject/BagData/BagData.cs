c6ade2a2f446889148345f6ca01ce2163ff5fc52
liangyufeng <2085462714@qq.com>
2025-07-09 17:56:46 +0800

    调整了部分命名规则

--- Assets/Scripts/ScritableObject/BagData/BagData.cs
+++ Assets/Scripts/ScritableObject/BagData/BagData.cs
@@ -1,9 +1,9 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 
-//public class BagData : ScriptableObject
-//{
-//    public List<CardData> cardList;
-//}
+public class BagData : ScriptableObject
+{
+    public List<CardData> cardList;
+}
