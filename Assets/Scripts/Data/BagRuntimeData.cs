9f181587ba127b565ac6601bb55e68a5221f2874
liangyufeng <2085462714@qq.com>
2025-07-09 17:29:55 +0800

    新增运行时的背包数据类

---
+++ Assets/Scripts/Data/BagRuntimeData.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+/// <summary>
+/// 背包运行时数据
+/// </summary>
+public class BagRuntimeData
+{
+    public int firstLoad; // 是否是第一次启动游戏
+
+    ///// <summary>
+    ///// 当前格子数量
+    ///// </summary>
+    //public int slotCount = 9;
+
+    ///// <summary>
+    ///// 
+    ///// </summary>
+    //public List<int> notEmptyCardSlotIndecis = new();
+
+    /// <summary>
+    /// 列表的长度代表了背包当前的格子数量，每一项代表这一格里的卡牌内容
+    /// </summary>
+    public List<CardSlotRuntimeData> cardSlotsRuntimeData = new()
+    {
+        // 初始有9个空格子
+        new CardSlotRuntimeData(), // 1
+        new CardSlotRuntimeData(), // 2
+        new CardSlotRuntimeData(), // 3
+        new CardSlotRuntimeData(), // 4
+        new CardSlotRuntimeData(), // 5
+        new CardSlotRuntimeData(), // 6
+        new CardSlotRuntimeData(), // 7
+        new CardSlotRuntimeData(), // 8
+        new CardSlotRuntimeData(), // 9
+    };
+}
--- Assets/Scripts/Data/GameDataManager.cs
+++ Assets/Scripts/Data/GameDataManager.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Assets.Scripts.Data
+﻿public class GameDataManager
 {
-    internal class GameDataManager
+    private static GameDataManager instance = new();
+    public static GameDataManager Instance => instance;
+
+    public PlayerBagRuntimeData PlayerBagRuntimeData { get; private set; }
+
+    public GameDataManager()
     {
+        PlayerBagRuntimeData = new();
+        EnvironmentBagRuntimeData = new();
+    }
+
+    public void RecordPlayerBagRuntimeData(PlayerBagRuntimeData runtimeData)
+    {
+        this.PlayerBagRuntimeData = runtimeData;
+    }
+
+
+    public EnvironmentBagRuntimeData EnvironmentBagRuntimeData { get; private set; }
+
+    public void RecordEnvironmentBagRuntimeData(EnvironmentBagRuntimeData runtimeData)
+    {
+        this.EnvironmentBagRuntimeData = runtimeData;
     }
 }
