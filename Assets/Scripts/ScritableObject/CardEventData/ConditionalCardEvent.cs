7d4704349efd83d964ebb22236d6039050d1b3e4
liangyufeng <2085462714@qq.com>
2025-07-10 22:46:51 +0800

    格式化代码

--- Assets/Scripts/ScritableObject/CardEventData/ConditionalCardEvent.cs
+++ Assets/Scripts/ScritableObject/CardEventData/ConditionalCardEvent.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "ConditionCardEvent", menuName = "ScritableObject/ConditionCardEvent")]
-public class ConditionalCardEvent:CardEvent
+public class ConditionalCardEvent : CardEvent
 {
-        public List<ConditionData> ConditionCardList;//list之间是或的关系
+    public List<ConditionData> ConditionCardList;//list之间是或的关系
 }
