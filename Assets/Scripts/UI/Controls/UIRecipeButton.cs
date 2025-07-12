ef60e4a6e08ca48e8512810d4a41177989dfa8a3
liangyufeng <2085462714@qq.com>
2025-07-12 21:48:54 +0800

    新增制作窗口中需要遇到的组件的脚本和预制体

---
+++ Assets/Scripts/UI/Controls/UIRecipeButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIRecipeButton : MonoBehaviour
+{
+    [SerializeField] Image icon;
+    [SerializeField] Image canCraft;
+    [SerializeField] Image isLocked;
+
+    public void DisplayRecipe(Sprite icon, bool locked, bool canCraft)
+    {
+        this.icon.sprite = icon;
+        if (!canCraft)
+        {
+            this.canCraft.gameObject.SetActive(false);
+            this.isLocked.gameObject.SetActive(false);
+        }
+        else
+        {
+            isLocked.gameObject.SetActive(locked);
+        }
+    }
+}
