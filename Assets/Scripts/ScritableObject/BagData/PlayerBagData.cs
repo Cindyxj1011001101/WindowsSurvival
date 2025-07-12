d69dde6aabd86a0fa3c94646b07652597957f0dd
liangyufeng <2085462714@qq.com>
2025-07-12 21:43:45 +0800

    删除bagdata相关

--- Assets/Scripts/ScritableObject/BagData/PlayerBagData.cs
+++
@@ -1,7 +0,0 @@
-﻿//using UnityEngine;
-
-//[CreateAssetMenu(fileName = "PlayerBag", menuName = "ScritableObject/PlayerBag")]
-//public class PlayerBagData : BagData
-//{
-//    public float maxLoad;
-//}
