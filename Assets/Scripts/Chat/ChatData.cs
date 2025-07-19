c0c7587370579b5792281e35f0cffe53ce05215b
Cindyxj1011001101 <13564548694@163.com>
2025-07-19 22:27:33 +0800

    初步对话

--- Assets/Scripts/Chat/ChatData.cs
+++ Assets/Scripts/Chat/ChatData.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using UnityEngine;
 public enum MessageSenderEnum
 {
     NPC,//NPC
@@ -8,19 +9,25 @@ public enum MessageSenderEnum
 
 public class ChatData
 {
-    public string MessageID;//消息ID
-    public string ParagraphID;//段落ID
+    public int MessageID;//消息ID
+    public int ParagraphID;//段落ID
     public MessageSenderEnum MessageSender;//消息发送者
-    public string MessageType;//消息类型（对话/选项）
+    public int MessageType;//消息类型（对话/选项）
     public string Message;//消息内容文本
+    public int NextMessageID;//下一条消息ID
     public int WaitTime;//播放本句后的等待时间
     public string TriggerMessageEffect;//消息触发时效果
     public ChatData(string[] line)
     {
-        MessageID = line[0];
-        ParagraphID = line[1];
-        MessageSender = (MessageSenderEnum)int.Parse(line[2]);
-        MessageType = line[3];
+        MessageID = int.Parse(line[0]);
+        ParagraphID = int.Parse(line[1]);
+        MessageSender = (MessageSenderEnum)(int.Parse(line[2])-1);
+        MessageType = int.Parse(line[3]);
+        Message=line[4];
+        if(line[5]!="")NextMessageID=int.Parse(line[5]);
+        else NextMessageID=-1;
+        WaitTime=int.Parse(line[7]);
+        TriggerMessageEffect=line[8];
     }
 }
 
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -4,7 +4,7 @@ public class Main : MonoBehaviour
 {
     void Start()
     {
-        Debug.Log(Application.persistentDataPath);
-        CSVReader.ReadChatData("test");
+        //Debug.Log(Application.persistentDataPath);
+
     }
 }
