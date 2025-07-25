e1ce5c898bb504c37fa6a60c2b249354c7e2c02f
liangyufeng <2085462714@qq.com>
2025-07-25 17:02:32 +0800

    修复鼠标进入离开后按钮颜色变亮的问题

--- Assets/Scripts/UI/Controls/UIRecipeMaterial.cs
+++ Assets/Scripts/UI/Controls/UIRecipeMaterial.cs
@@ -6,11 +6,12 @@ public class UIRecipeMaterial : MonoBehaviour
     [SerializeField] private Image icon;
     [SerializeField] private Text requiredNumText;
     [SerializeField] private Color notAdequateColor;
+    [SerializeField] private HoverableButton button;
 
     public void DisplayMaterial(Sprite icon, int requiredNum, int currentNum)
     {
         this.icon.sprite = icon;
         requiredNumText.text = $"{currentNum}/{requiredNum}";
-        this.icon.color = requiredNumText.color = currentNum < requiredNum ? notAdequateColor : Color.white;
+        button.currentColor = this.icon.color = requiredNumText.color = currentNum < requiredNum ? notAdequateColor : Color.white;
     }
 }
