db1f92c5de478812b51b30c845b41bfe4c54569d
liangyufeng <2085462714@qq.com>
2025-08-03 22:44:03 +0800

    修复状态图标的警示动效有时候无法播放的问题

--- Assets/Scripts/UI/Controls/UIState/UIStateSlider.cs
+++ Assets/Scripts/UI/Controls/UIState/UIStateSlider.cs
@@ -42,6 +42,8 @@ public class UIStateSlider : MonoBehaviour
 
     private void PlayerStateDangerAlert(DangerLevelEnum dangerLevel)
     {
+        button.transform.DOKill();
+        button.transform.localScale = Vector3.one;
         switch (dangerLevel)
         {
             case DangerLevelEnum.High:
@@ -51,15 +53,12 @@ public class UIStateSlider : MonoBehaviour
                 IconSizeYoloTween(1.1f, .5f);
                 break;
             case DangerLevelEnum.None:
-                button.transform.DOKill();
-                button.transform.localScale = Vector3.one;
                 break;
         }
     }
 
     private void IconSizeYoloTween(float scaleSize, float duration)
     {
-        button.transform.DOKill();
         button.transform.DOScale(scaleSize, duration)
             .SetLoops(-1, LoopType.Yoyo) // 无限循环，来回播放
             .SetEase(Ease.InOutSine);    // 设置缓动效果
