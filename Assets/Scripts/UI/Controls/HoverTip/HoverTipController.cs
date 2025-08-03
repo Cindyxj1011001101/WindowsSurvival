4877db9a5f0526ef40b6fabbf667dbf2a8c9d0b3
liangyufeng <2085462714@qq.com>
2025-08-03 15:33:16 +0800

    使按钮的悬浮提示始终显示在屏幕范围内

--- Assets/Scripts/UI/Controls/HoverTip/HoverTipController.cs
+++ Assets/Scripts/UI/Controls/HoverTip/HoverTipController.cs
@@ -32,9 +32,7 @@ public class HoverTipController : MonoBehaviour, IPointerEnterHandler, IPointerE
     private void ShowTip()
     {
         // 设置位置
-        var rect = transform as RectTransform;
-        hoverTip.transform.position = transform.position +
-            new Vector3(rect.sizeDelta.x / 2 + offset.x, rect.sizeDelta.y / 2 + offset.y);
+        hoverTip.transform.position = CalcTipPos();
 
         hoverTip.Show();
     }
@@ -48,5 +46,44 @@ public class HoverTipController : MonoBehaviour, IPointerEnterHandler, IPointerE
     {
         if (hoverTip != null)
             hoverTip.SelfDestroy();
+    }
+
+    private Vector3 CalcTipPos()
+    {
+        // 获取tip的尺寸
+        var tipRect = hoverTip.transform as RectTransform;
+        Vector2 tipSize = tipRect.sizeDelta * tipRect.lossyScale;
+
+        // 默认锚点是左上角
+        var pivot = tipRect.pivot = new Vector2(0, 1);
+
+        // 获取屏幕坐标
+        var canvas = WindowsManager.Instance.GetComponentInParent<Canvas>();
+        var rect = transform as RectTransform;
+        var worldPos = rect.position + new Vector3(rect.sizeDelta.x / 2 + offset.x, rect.sizeDelta.y / 2 + offset.y);
+        Vector2 screenPos = RectTransformUtility.WorldToScreenPoint(canvas.worldCamera, worldPos);
+
+        if (screenPos.x + tipSize.x > Screen.width)
+        {
+            pivot.x = 1;
+            tipRect.pivot = pivot;
+            worldPos.x = rect.position.x - rect.sizeDelta.x /2 - offset.x;
+        }
+        if (screenPos.y - tipSize.y < 0)
+        {
+            pivot.y = 0;
+            tipRect.pivot = pivot;
+            worldPos.y = rect.position.y - rect.sizeDelta.y / 2 - offset.y;
+        }
+        if (screenPos.y > Screen.height)
+        {
+            Debug.Log("超过下边界");
+        }
+        if (screenPos.x < 0)
+        {
+            Debug.Log("超过左边界");
+        }
+
+        return worldPos;
     }
 }
