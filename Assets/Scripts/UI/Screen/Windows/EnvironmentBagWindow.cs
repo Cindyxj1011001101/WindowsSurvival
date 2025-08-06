8408dc26241df87a5ed7e0285f68812689fbd0b4
liangyufeng <2085462714@qq.com>
2025-08-06 17:29:44 +0800

    添加沙漏转动动画

--- Assets/Scripts/UI/Screen/Windows/EnvironmentBagWindow.cs
+++ Assets/Scripts/UI/Screen/Windows/EnvironmentBagWindow.cs
@@ -145,6 +145,13 @@ public class EnvironmentBagWindow : BagWindow
 
         // 显示探索按钮
         exploreButton.onClick.RemoveAllListeners(); // 清除之前的监听器
+
+        // 播放鼠标等待动画
+        exploreButton.onClick.AddListener(() =>
+        {
+            MouseManager.Instance.Wait();
+        });
+
         var text = exploreButton.GetComponentInChildren<Text>();
         if (args.completed)
         {
