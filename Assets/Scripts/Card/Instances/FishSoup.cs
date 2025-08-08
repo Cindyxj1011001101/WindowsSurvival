c06fd038c45e28f94e6119c7972fee94fe482454
Cindyxj1011001101 <13564548694@163.com>
2025-08-08 16:00:59 +0800

    增加3张卡牌类

---
+++ Assets/Scripts/Card/Instances/FishSoup.cs
@@ -0,0 +1,22 @@
+public class FishSoup : Card
+{
+    private FishSoup()
+    {
+        Events=new()
+        {
+            new Event("食用", "食用鱼汤", Event_Eat, null)
+        };
+    }
+
+    public void Event_Eat(out string tip)
+    {
+        tip = string.Empty;
+        DestroyThis();
+        TimeManager.Instance.AddTime(15);
+        StateManager.Instance.ChangePlayerState(PlayerStateEnum.Fullness, 10);
+        StateManager.Instance.ChangePlayerState(PlayerStateEnum.Thirst, 18);
+        StateManager.Instance.ChangePlayerState(PlayerStateEnum.San, 12);
+        StateManager.Instance.ChangePlayerState(PlayerStateEnum.Health, 12);
+        StateManager.Instance.ChangePlayerState(PlayerStateEnum.PainLevel, -25);
+    }
+}
