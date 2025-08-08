78e3430fb15b8b410567a07115eb9927a64021ff
liangyufeng <2085462714@qq.com>
2025-08-08 20:20:14 +0800

    给废料堆加一个用铲子挖掘的行为

--- Assets/Scripts/Card/Instances/WasteHeap.cs
+++ Assets/Scripts/Card/Instances/WasteHeap.cs
@@ -9,7 +9,8 @@ public class WasteHeap : Card
     {
         Events = new()
         {
-            new Event("用手挖掘", "这会费时费力", Event_Dig, null, () => 45)
+            new Event("用手挖掘", "这会费时费力", Event_Dig, null, () => 45),
+            new Event("用铲子挖掘", "比用手轻松一些", Event_Dig, Judge_DigByTool, () => 15),
         };
     }
 
@@ -25,7 +26,34 @@ public class WasteHeap : Card
         TimeManager.Instance.AddTime(45);
         //掉落卡牌
         RandomDrop();
+    }
 
+    public void Event_DigByTool(out string tip)
+    {
+        tip = string.Empty;
+        if (SoundManager.Instance != null)
+            SoundManager.Instance.PlaySound("挖掘废料_01", true);
+
+        //消耗1点耐久度
+        TryUse();
+        // 工具消耗耐久
+        var card = GameManager.Instance.PlayerBag.FindCardOfToolType(ToolType.Dig);
+        card.TryUse();
+        //消耗45分钟
+        TimeManager.Instance.AddTime(15);
+        //掉落卡牌
+        RandomDrop();
+    }
+
+    private bool Judge_DigByTool(out string hint)
+    {
+        hint = string.Empty;
+        if (GameManager.Instance.PlayerBag.FindCardOfToolType(ToolType.Dig) == null)
+        {
+            hint = "需要挖掘类工具";
+            return false;
+        }
+        return true;
     }
 
     public void RandomDrop()
