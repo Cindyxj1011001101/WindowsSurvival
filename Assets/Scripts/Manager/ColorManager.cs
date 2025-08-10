5c29af200283b53426fa52af9a4017628af8701d
huajiian <1150787367@qq.com>
2025-08-10 14:34:27 +0800

    修改colormanager中DarkGrey颜色

--- Assets/Scripts/Manager/ColorManager.cs
+++ Assets/Scripts/Manager/ColorManager.cs
@@ -3,7 +3,7 @@
 public static class ColorManager
 {
     public static Color32 Black { get; private set; } = new(17, 17, 17, 255);
-    public static Color32 DarkGrey { get; private set; } = new(41, 41, 41, 255);
+    public static Color32 DarkGrey { get; private set; } = new(90, 90, 90, 255);
     public static Color32 LightGrey { get; private set; } = new(118, 118, 118, 255);
     public static Color32 White { get; private set; } = new(255, 255, 255, 255);
     public static Color32 Blue { get; private set; } = new(77, 154, 255, 255);
