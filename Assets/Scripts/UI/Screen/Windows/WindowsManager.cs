4e19e6509c6d83c9519c67ad168a6585203bcf89
liangyufeng <2085462714@qq.com>
2025-08-18 14:39:13 +0800

    将布局预设按钮移至右上角

--- Assets/Scripts/UI/Screen/Windows/WindowsManager.cs
+++ Assets/Scripts/UI/Screen/Windows/WindowsManager.cs
@@ -81,7 +81,7 @@ public class WindowsManager : MonoBehaviour
             button.onClick.AddListener(() =>
             {
                 var window = (OpenWindow("Confirm", true) as ConfirmWindow);
-                window.SetText("是否要应用" + buttonText + "？"); // 使用局部变量
+                window.SetText("是否要应用窗口布局预设" + buttonText + "？"); // 使用局部变量
                 window.onConfirm += () =>
                 {
                     ApplyPreset(index); // 使用局部变量 index
