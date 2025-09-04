5bce248188316c9fd3093d3b545ae7a35b72763b
liangyufeng <2085462714@qq.com>
2025-09-04 20:51:10 +0800

    修复一次性添加多张卡牌时的显示问题

--- Assets/Scripts/Utils/MFXUtility.cs
+++ Assets/Scripts/Utils/MFXUtility.cs
@@ -44,7 +44,7 @@ public class MFXUtility
     public static CardSlot CreateSlot(Vector2 screenPosition)
     {
         // 实例化预制体
-        GameObject slotObj = ObjectBufferPool.Instance.Get("Prefabs/UI/Controls/CardSlot", "CardSlot", WindowsManager.Instance.TempCardSlotLayer);
+        GameObject slotObj = ObjectBufferPool.Instance.Get("Prefabs/UI/Controls/CardSlot", "TempCardSlot", WindowsManager.Instance.TempCardSlotLayer);
 
         // 获取RectTransform并设置位置
         RectTransform slotRect = slotObj.GetComponent<RectTransform>();
@@ -111,10 +111,10 @@ public class MFXUtility
     {
         var seq = DOTween.Sequence();
 
-        Card card;
+        
         for (int i = 0; i < cards.Count; i++)
         {
-            card = cards[i];
+            Card card = cards[i];
             seq.Join(MoveCard(
                 card,
                 1,
