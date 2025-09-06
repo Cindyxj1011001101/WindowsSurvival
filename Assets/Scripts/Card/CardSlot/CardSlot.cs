8e03888b705be2f09f3ec9fb57ca0a6946312705
liangyufeng <2085462714@qq.com>
2025-09-06 19:42:21 +0800

    修复拆毁建筑时的空异常和竖的卡牌组件显示位置错误

--- Assets/Scripts/Card/CardSlot/CardSlot.cs
+++ Assets/Scripts/Card/CardSlot/CardSlot.cs
@@ -199,14 +199,14 @@ public class CardSlot : MonoBehaviour
                 slider = ObjectBufferPool.Instance.Get("Prefabs/UI/Controls/Components", "TimerComponent", parent).GetComponent<UIStateSlider>();
             else
                 slider = ObjectBufferPool.Instance.Get("Prefabs/UI/Controls/Components", $"{(vertical ? "Vertical" : "")}Component", parent).GetComponent<UIStateSlider>();
-        }
 
-        componentSliders.Add(component.GetType(), slider);
+            componentSliders.Add(component.GetType(), slider);
+        }
 
         slider.transform.SetAsLastSibling();
 
-        (slider.transform as RectTransform).anchoredPosition = Vector3.zero;
         slider.transform.position = parent.position;
+        (slider.transform as RectTransform).anchoredPosition = Vector3.zero;
         slider.transform.localRotation = Quaternion.identity;
 
         if (ColorManager.CardComponentColors.TryGetValue(component.GetType(), out var fillColor))
