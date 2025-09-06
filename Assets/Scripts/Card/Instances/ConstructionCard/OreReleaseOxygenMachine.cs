bd08df5f1a9121e48fb5b1b47c5483bc4daa4e9e
liangyufeng <2085462714@qq.com>
2025-09-06 13:43:10 +0800

    修复矿石释氧机不能快捷拆毁的问题

--- Assets/Scripts/Card/Instances/ConstructionCard/OreReleaseOxygenMachine.cs
+++ Assets/Scripts/Card/Instances/ConstructionCard/OreReleaseOxygenMachine.cs
@@ -70,11 +70,19 @@ public class OreReleaseOxygenMachine : ConstructionCard
 
     public override bool CanQuickInteract(Card card)
     {
+        if (base.CanQuickInteract(card)) return true;
+
         return innerContents.CanQuickInteract(card);
     }
 
     public override void QuickIneract(SlotCards slot, int count, out string tip)
     {
+        if (base.CanQuickInteract(slot.PeekCard()))
+        {
+            base.QuickIneract(slot, count, out tip);
+            return;
+        }
+
         innerContents.QuickIneract(slot, count, out tip);
     }
 
