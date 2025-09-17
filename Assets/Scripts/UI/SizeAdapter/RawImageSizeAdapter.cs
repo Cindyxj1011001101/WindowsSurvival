2297a4404ed84f049d414c67e8152af3bd053d85
Cindyxj1011001101 <13564548694@163.com>
2025-09-17 17:50:10 +0800

    待机动画导入与窗口适配

--- Assets/Scripts/UI/SizeAdapter/RawImageSizeAdapter.cs
+++ Assets/Scripts/UI/SizeAdapter/RawImageSizeAdapter.cs
@@ -12,7 +12,7 @@ public class RawImageSizeAdapter : MonoBehaviour, IAdaptiveSize
 
     private void Awake()
     {
-        rawImage = GetComponent<RawImage>();
+        // rawImage = GetComponent<RawImage>();
         rectTransform = GetComponent<RectTransform>();
         parentTransform = transform.parent.GetComponent<RectTransform>();
 
@@ -29,16 +29,18 @@ public class RawImageSizeAdapter : MonoBehaviour, IAdaptiveSize
     {
         float currentWidth = parentTransform.rect.width;
         float currentHeight = parentTransform.rect.height;
+        
+        float min = Mathf.Min(currentWidth, currentHeight);
+        rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, min);
+        rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, min);
 
-        rectTransform.sizeDelta = new Vector2(currentWidth, currentHeight);
-
-        var uvRect = new Rect();
-
-        uvRect.width = currentWidth / initialWidth;
-        uvRect.height = currentHeight / initialHeight;
-        uvRect.x = -(currentWidth / 2 - initialWidth / 2) / initialWidth;
-        uvRect.y = -(currentHeight / 2 - initialHeight / 2) / initialHeight;
-
-        rawImage.uvRect = uvRect;
+        // var uvRect = new Rect();
+        //
+        // uvRect.width = currentWidth / initialWidth;
+        // uvRect.height = currentHeight / initialHeight;
+        // uvRect.x = -(currentWidth / 2 - initialWidth / 2) / initialWidth;
+        // uvRect.y = -(currentHeight / 2 - initialHeight / 2) / initialHeight;
+        //
+        // rawImage.uvRect = uvRect;
     }
 }
