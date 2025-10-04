16d02ed01301d89b9c7e776c4a5ef1f814ff5b73
liangyufeng <2085462714@qq.com>
2025-10-04 14:22:32 +0800

    添加新的卡牌类型和tag

--- Assets/Scripts/Card/Instances/Card.cs
+++ Assets/Scripts/Card/Instances/Card.cs
@@ -9,23 +9,27 @@ using UnityEngine.Events;
 public enum CardTag
 {
     Rubbish, // 垃圾
+    Plants,  // 素食
+    Meat,    // 肉食
 }
 
 public enum CardType
 {
-    Place, // 地点
+    Place,         // 地点
     ResourcePoint, // 资源点
-    Creature, // 生物
-    Crop, // 作物
-    Seed, // 种子
-    Construction, // 建筑
-    Food, // 食物
-    Liquids​, // 液体
-    Medicine​, // 药品
-    Resource, // 资源
-    Tool, // 工具
-    Equipment, // 装备
-    Other, // 其他
+    LiveEntities,  // 实体
+    Creature,      // 生物
+    Crop,          // 作物
+    Seed,          // 种子
+    Construction,  // 建筑
+    Food,          // 食物
+    Liquids​,       // 液体
+    Medicine​,      // 药品
+    Resource,      // 资源
+    Tool,          // 工具
+    Weapon,        // 武器
+    Equipment,     // 装备
+    Other,         // 其他
 }
 
 /// <summary>
