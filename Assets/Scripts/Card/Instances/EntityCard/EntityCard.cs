f723ca24d8df35ba6f375b2703e3f903a4d5a3d1
liangyufeng <2085462714@qq.com>
2025-10-13 09:14:09 +0800

    实现实体的被攻击逻辑

--- Assets/Scripts/Card/Instances/EntityCard/EntityCard.cs
+++ Assets/Scripts/Card/Instances/EntityCard/EntityCard.cs
@@ -28,4 +28,23 @@
     {
         EventManager.Instance.RemoveListener(EventType.PlayerMove, RefreshSlot);
     }
+
+    public override bool CanQuickInteract(Card card, out string tip)
+    {
+        tip = string.Empty;
+        if (card.TryGetComponent<WeaponComponent>(out var weapon) && weapon.WithinAttackRange(this))
+        {
+            tip = $"攻击该单位\n耗时:  {weapon.attackTime}分钟\n造成伤害:  {weapon.atk}";
+            return true;
+        }
+        return false;
+    }
+
+    public override void QuickIneract(SlotCards slot, int count, out string tip)
+    {
+        tip = string.Empty;
+        var card = slot.PeekCard();
+        card.TryGetComponent<WeaponComponent>(out var weapon);
+        weapon.DealDamage(this);
+    }
 }
