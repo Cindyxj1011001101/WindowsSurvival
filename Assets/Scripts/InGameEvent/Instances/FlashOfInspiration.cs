9074c098f01e47a2899c72b86698fcc49b9c93bc
liangyufeng <2085462714@qq.com>
2025-10-18 11:26:40 +0800

    完成灵光乍现事件逻辑

--- Assets/Scripts/InGameEvent/Instances/FlashOfInspiration.cs
+++ Assets/Scripts/InGameEvent/Instances/FlashOfInspiration.cs
@@ -3,8 +3,20 @@
 /// </summary>
 public class FlashOfInspiration : InGameEvent
 {
+    private const float SAN_THRESHOLD = 0.85f; // 精神状态阈值
+
+    public override bool CanTriggerThisEvent()
+    {
+        var san = StateManager.Instance.PlayerStateDict[PlayerStateEnum.San];
+        return san.CurValue / san.MaxValue >= SAN_THRESHOLD;
+    }
+
     public override void TriggerThisEvent()
     {
-        
+        // 立刻完成当前科技
+        if (TechnologyManager.Instance.CurStudiedTechNode != null)
+        {
+            TechnologyManager.Instance.AddStudyProcess(9999); // 研究进度增加
+        }
     }
 }
