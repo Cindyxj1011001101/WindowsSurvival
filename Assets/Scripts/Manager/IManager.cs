c6d3910a04054909e8b84e407fd01454da4f16ad
liangyufeng <2085462714@qq.com>
2025-10-27 22:23:30 +0800

    新增接口IManager，游戏内不必继承MonoBehaviour的管理器实现它，用来统一管理器的初始化和重置

---
+++ Assets/Scripts/Manager/IManager.cs
@@ -0,0 +1,11 @@
+﻿public interface IManager
+{
+    /// <summary>
+    /// 初始化。进入新存档时调用
+    /// </summary>
+    void Init();
+    /// <summary>
+    /// 重置。离开当前存档时调用
+    /// </summary>
+    void Reset();
+}
