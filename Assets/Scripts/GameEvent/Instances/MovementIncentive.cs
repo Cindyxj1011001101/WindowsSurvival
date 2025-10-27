aae01fed05f502253929f50a843678843f26a49d
liangyufeng <2085462714@qq.com>
2025-10-27 15:14:29 +0800

    完成移动激励事件

--- Assets/Scripts/GameEvent/Instances/MovementIncentive.cs
+++ Assets/Scripts/GameEvent/Instances/MovementIncentive.cs
@@ -5,12 +5,17 @@ public class MovementIncentive : GameEvent
 {
     public override string GetDetails()
     {
-        return @"麦麦最近精神很好，连游泳和跑步都变快了不少。
-                 在接下来的一段时间里，麦麦-50%移动时长。";
+        return $"麦麦最近精神很好，连游泳和跑步都变快了不少。\n\n" +
+               $"在接下来的一段时间里，麦麦 {ColorManager.Colorize("-50%", ColorManager.Green)} 移动时长。";
     }
 
     public override void OnTrigger()
     {
-        
+        GameManager.Instance.AddMoveExtraEffect("移动激励", -0.5f, null);
+    }
+
+    public override void OnEnd()
+    {
+        GameManager.Instance.RemoveMoveExtraEffect("移动激励");
     }
 }
