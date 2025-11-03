9133e2df58e93ec79cab2aab714711de7e812ca5
liangyufeng <2085462714@qq.com>
2025-11-03 14:28:06 +0800

    素食tag改名为vege，猎人tag改名为predator

--- Assets/Scripts/Card/CardTag.cs
+++ Assets/Scripts/Card/CardTag.cs
@@ -1,9 +1,9 @@
 ﻿public enum CardTag
 {
     Rubbish,    // 垃圾
-    Plants,     // 素食
+    Vege,       // 素食
     Meat,       // 肉食
-    Hunter,     // 猎人
+    Predator,   // 掠食者
     Food,       // 食物
     FruitCrop,  // 果实作物
 }
