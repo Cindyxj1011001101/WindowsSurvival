04543a075b82059b5a895e1d1666afee41cd03d4
liangyufeng <2085462714@qq.com>
2025-11-05 13:11:31 +0800

    将初始化管理器的代码移至Main脚本；将Main脚本从StartScene移除

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Main : MonoBehaviour
@@ -8,12 +9,46 @@ public class Main : MonoBehaviour
 
     public int targetFrameRate = 60;
 
-    void Start()
+    private List<IManager> orderedManagers = new()
     {
-        // Debug.Log(Application.persistentDataPath);
+        // 按顺序列出所有需要初始化的Manager
+        UpdateManager.Instance,
+        GlobalDataManager.Instance,
+        TimeManager.Instance,
+        SunlightManager.Instance,
+        TechnologyManager.Instance,
+        CraftManager.Instance,
+        GameEventManager.Instance,
+        Player.Instance,
+        StateManager.Instance,
+        GameManager.Instance,
+        MoveExploreManager.Instance,
+    };
 
+    private void Awake()
+    {
+        // 设置分辨率
         SetResolution();
         //SetFrameRate();
+
+        // 初始化各类管理器
+        InitManagers();
+    }
+
+    private void OnDestroy()
+    {
+        foreach (var m in orderedManagers)
+        {
+            m.Reset();
+        }
+    }
+
+    private void InitManagers()
+    {
+        foreach (var m in orderedManagers)
+        {
+            m.Init();
+        }
     }
 
     private void SetResolution()
