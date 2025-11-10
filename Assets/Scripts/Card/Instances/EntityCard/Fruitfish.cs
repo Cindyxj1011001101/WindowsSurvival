88b39373c4d08b4c13895f41ca467efc998e11a8
liangyufeng <2085462714@qq.com>
2025-11-10 12:06:27 +0800

    修改食果鲀逻辑以适应之前的修改

--- Assets/Scripts/Card/Instances/EntityCard/Fruitfish.cs
+++ Assets/Scripts/Card/Instances/EntityCard/Fruitfish.cs
@@ -10,25 +10,32 @@ public class Fruitfish : EntityCard
         AddCardEvent("用捞网捉", "", Event_CatchByNet, Judge_CatchByNet, () => 15);
     }
 
-    protected override string GetHighestPriorityIntention()
+    protected override string GetHighestPriorityIntention(out object[] cache)
     {
+        cache = null;
         var env = Bag as EnvironmentBag;
+        
         // 地上有素食卡牌
-        if (!env.FindCardsOfTag(CardTag.Vege).IsNullOrEmpty())
+        var vegeCards = env.FindCardsOfTag(CardTag.Vege);
+        if (!vegeCards.IsNullOrEmpty())
         {
+            cache = new object[] { vegeCards.GetRandomly().Uuid };
             return "食用";
         }
 
         // 离predator的最近距离小于7
         var closestPredator = GetClosestPredator();
-        if (closestPredator != null && closestPredator.DistanceTo(this) < 7)
+        if (closestPredator != null && DistanceTo(closestPredator) < 7)
         {
+            cache = new object[] { closestPredator.Uuid };
             return "逃跑";
         }
 
         // 地点有成熟的果实作物
-        if (!GetRipeFruitCrops().IsNullOrEmpty())
+        var fruitCards = GetRipeFruitCrops();
+        if (!fruitCards.IsNullOrEmpty())
         {
+            cache = new object[] { fruitCards.GetRandomly().Uuid };
             return "采摘并食用";
         }
 
@@ -42,48 +49,45 @@ public class Fruitfish : EntityCard
         AddIntention("采摘并食用", 15, Intention_PickAndEat);
     }
 
-    private void Intention_Eat()
+    private void Intention_Eat(object[] cache)
     {
         // 食果鲀会随机选择该地点的地上的一张带有素食tag的卡牌吃掉
-        var env = Bag as EnvironmentBag;
-        // 获取当前地点的所有素食卡牌
-        var foodCards = env.FindCardsOfTag(CardTag.Vege);
+        var cardUuid = cache[0] as string;
+        var toEat = GlobalDataManager.Instance.GetCardByUuid(cardUuid);
 
-        // 没有素食卡牌，意图执行失败
-        if (foodCards.IsNullOrEmpty()) return;
+        // 食物已不存在，意图执行失败
+        if (toEat == null) return;
 
-        // 随机选择一张素食卡牌
-        var selected = foodCards[UnityEngine.Random.Range(0, foodCards.Count)];
+        // 食物不在当前地点，意图执行失败
+        if (!IsInSameBag(toEat)) return;
+
         // 吃掉
         // TODO: 吃掉动效
-        selected.DestroyThis();
+        toEat.DestroyThis();
     }
 
-    private void Intention_Escape()
+    private void Intention_Escape(object[] cache)
     {
         // 往最近的（带猎人tag的卡牌或玩家）的反方向移动(准备时间*移动速度)的距离。
         // 如果移动后的坐标超过了该地点的坐标范围，则判断该地点是否是室内地点。如是，则不会超出坐标范围而是停在范围边界。如不是，则弹出“食果鲀逃走了”的提示，并销毁食果鲀。
-        var env = Bag as EnvironmentBag;
-        
-        // 找到最近的predator
-        var closetPredator = GetClosestPredator();
 
-        // 没有predator，意图执行失败
+        var targetUuid = cache[0] as string;
+        var closetPredator = GlobalDataManager.Instance.GetEntityByUuid(targetUuid);
+
+        // 目标已不存在，意图执行失败
         if (closetPredator == null) return;
 
         // 向远离最近的predator的方向逃离
-        this.MoveAwayFrom(closetPredator, moveDistPerMin * intentions["逃跑"].PreparationMinutes);
+        MoveAwayFrom(closetPredator, CurrentIntention.PreparationMinutes * moveDistPerMin);
 
+        var env = Bag as EnvironmentBag;
         // 如果逃离到边界并且当前地点不是室内地点
         if (Coordinate.IsAtBoundary && !env.PlaceData.isIndoor)
         {
             // 食果鲀消失
             DestroyThis();
             ShowTip($"{CardName}逃跑了");
-            return;
         }
-
-        RefreshSlot();
     }
 
     private IEntity GetClosestPredator()
@@ -98,17 +102,17 @@ public class Fruitfish : EntityCard
         {
             if (c is not IEntity entity) continue;
 
-            var dist = this.DistanceTo(entity);
+            var dist = DistanceTo(entity);
             if (dist >= closetDist) continue;
 
             closetDist = dist;
             closetPredator = entity;
         }
         // 如果玩家和食果鲀在同一地点
-        if (this.IsInSameLocation(Player.Instance))
+        if (IsInSameLocation(Player.Instance))
         {
             // 将玩家也加入判断
-            var dist = this.DistanceTo(Player.Instance);
+            var dist = DistanceTo(Player.Instance);
             if (dist < closetDist)
             {
                 closetPredator = Player.Instance;
@@ -118,20 +122,21 @@ public class Fruitfish : EntityCard
         return closetPredator;
     }
 
-    private void Intention_PickAndEat()
+    private void Intention_PickAndEat(object[] cache)
     {
         // 食果鲀会随机选择该地点的一张“果实作物”tag的“是否成熟”为true的作物卡牌，使其是否成熟变为否，作物生长度变为0。
-        // 获取当前地点的所有成熟的果实作物
-        var ripeCards = GetRipeFruitCrops();
+        var cardUuid = cache[0] as string;
+        var toEat = GlobalDataManager.Instance.GetCardByUuid(cardUuid);
 
-        // 没有成熟的果实作物，意图执行失败
-        if (ripeCards.IsNullOrEmpty()) return;
+        // 食物已不存在，意图执行失败
+        if (toEat == null) return;
 
-        // 随机选择一个
-        var selected = ripeCards[UnityEngine.Random.Range(0, ripeCards.Count)] as PlantCard;
-        // 使其作物生长度变为0
-        // TODO: 动效
-        selected.SetPlantGrowth(0);
+        // 食物不在当前地点，意图执行失败
+        if (!IsInSameBag(toEat)) return;
+
+        // 吃掉
+        // TODO: 吃掉动效
+        (toEat as PlantCard).SetPlantGrowth(0);
     }
 
     private List<Card> GetRipeFruitCrops()
