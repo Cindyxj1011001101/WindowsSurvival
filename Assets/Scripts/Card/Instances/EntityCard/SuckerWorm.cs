be2baf56238f1466348f65ebbd09407b919430d6
liangyufeng <2085462714@qq.com>
2025-11-10 12:34:01 +0800

    修复吸盘蠕虫判断其他实体与自身距离时的比较运算符错误

--- Assets/Scripts/Card/Instances/EntityCard/SuckerWorm.cs
+++ Assets/Scripts/Card/Instances/EntityCard/SuckerWorm.cs
@@ -32,7 +32,7 @@ public class SuckerWorm : EntityCard
     {
         // 吸盘蠕虫会将距离其[0,5]的部分实体单位加入仇恨列表，优先级8，持续时间15分钟。
         // 包括以下单位：麦麦、食果鲀、裙水母、狮子水母、老鼠
-        if (potentialAggroEntityTypes.Contains(entity.GetType()) && DistanceTo(entity) > 5)
+        if (potentialAggroEntityTypes.Contains(entity.GetType()) && DistanceTo(entity) <= 5)
         {
             AddAggro(entity, 8, 15);
         }
