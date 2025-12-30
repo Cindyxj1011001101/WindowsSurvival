df6956dd2190512aebfc63ca4e1bcc7f2241fe6f
liangyufeng <2085462714@qq.com>
2025-12-30 23:09:19 +0800

    修复实体会以已不存在的实体为目标的问题

--- Assets/Scripts/Entity/Intention/SingleTargetIntention.cs
+++ Assets/Scripts/Entity/Intention/SingleTargetIntention.cs
@@ -3,31 +3,13 @@
 public abstract class SingleTargetIntention : EntityIntention
 {
     [JsonProperty] protected string targetUuid;
-    [JsonIgnore] private IEntity entityTarget;
-    [JsonIgnore] private Card cardTarget;
 
-    [JsonIgnore] protected IEntity EntityTarget
-    {
-        get
-        {
-            entityTarget ??= GlobalDataManager.Instance.GetEntityByUuid(targetUuid);
-            return entityTarget;
-        }
-    }
+    [JsonIgnore] protected IEntity EntityTarget => GlobalDataManager.Instance.GetEntityByUuid(targetUuid);
 
-    [JsonIgnore] protected Card CardTarget
-    {
-        get
-        {
-            cardTarget ??= GlobalDataManager.Instance.GetCardByUuid(targetUuid);
-            return cardTarget;
-        }
-    }
+    [JsonIgnore] protected Card CardTarget => GlobalDataManager.Instance.GetCardByUuid(targetUuid);
 
     protected SingleTargetIntention(int preparationMinutes, string targetUuid) : base(preparationMinutes)
     {
         this.targetUuid = targetUuid;
-        entityTarget = GlobalDataManager.Instance.GetEntityByUuid(targetUuid);
-        cardTarget = GlobalDataManager.Instance.GetCardByUuid(targetUuid);
     }
 }
